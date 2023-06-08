9860d2048d6ae36fa976c1648efe026e8844a049
Jhonatan Veliz <jhontanveliz74@gmail.com>
2023-06-08 10:22:59 -0600

    modifed project carousel

--- js/view-data.js
+++ js/view-data.js
@@ -1,25 +1,32 @@
-  // Función para obtener los parámetros de la URL
-  function getParametrosURL() {
-    const parametros = {};
-    const url = window.location.href;
-    const inicio = url.indexOf('?');
+// Funcion que verifica si la url contiene parametros
+function verificarParametros(url, inicio, parametros) {
+  if (inicio !== -1) {
+    const cadenaParametros = url.slice(inicio + 1);
+    const paresParametros = cadenaParametros.split('&');
 
-    if (inicio !== -1) {
-      const cadenaParametros = url.slice(inicio + 1);
-      const paresParametros = cadenaParametros.split('&');
-
-      for (let i = 0; i < paresParametros.length; i++) {
-        const par = paresParametros[i].split('=');
-        const nombre = decodeURIComponent(par[0]);
-        const valor = decodeURIComponent(par[1]);
-        parametros[nombre] = valor;
-      }
-    }
-
-    return parametros;
+    getObjectParametros(paresParametros, parametros);
   }
+}
+// Encapsulación del codigo
+function getObjectParametros(paresParametros, parametros) {
+  for (const element of paresParametros) {
+    const par = element.split('=');
+    const nombre = decodeURIComponent(par[0]);
+    const valor = decodeURIComponent(par[1]);
+    parametros[nombre] = valor;
+  }
+}
+// Función para obtener los parámetros de la URL
+function getParametrosURL() {
+  const parametros = {};
+  const url = window.location.href;
+  const inicio = url.indexOf('?');
 
+  verificarParametros(url, inicio, parametros)
+  return parametros;
+}
+// entrando al objeto con nuevos valores que esta funcion generó
 const parametros = getParametrosURL();
-
+// Agregando los valores al DOM
 const content = document.querySelector('.content');
-content.data = parametros.certificateNormal;
+content.data = parametros.certificate;
--- js/view.js
+++ js/view.js
@@ -5,10 +5,23 @@ const certificateNormal = [
     {data: './files/c-productividad.pdf'},
 ];
 
+const certificateFormal = [
+    {data: './files/cf-html-css.pdf'},
+    {data: './files/cf-git.pdf'},
+    {data: './files/cf-productividad.pdf'},
+]
+
 const btnCertifyNormal = document.querySelectorAll('.certify-normal');
+const btnCertifyFormal = document.querySelectorAll('.certify-formal');
 
 btnCertifyNormal.forEach((boton, index)=>{
     boton.addEventListener('click', ()=>{
-        window.open('./view.html?certificateNormal=' + encodeURIComponent(certificateNormal[index].data), '_blank');
+        window.open('./view.html?certificate=' + encodeURIComponent(certificateNormal[index].data), '_blank');
+    });
+});
+
+btnCertifyFormal.forEach((boton, index)=>{
+    boton.addEventListener('click', ()=>{
+        window.open('./view.html?certificate=' + encodeURIComponent(certificateFormal[index].data), '_blank');
     });
 });
